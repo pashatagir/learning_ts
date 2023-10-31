a24cc64a8dfcf2aa1df1eb27734b01f7e6e432c2
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-10-31 19:23:05 +0200

    static method and properties

--- src/app.ts
+++ src/app.ts
@@ -270,3 +270,30 @@ person.firstName = "Petha";
 person.lastName = "Pupkin";
 
 console.log(person.info);
+
+// ?static method and properties
+class UseStatic {
+  private static count = 0;
+
+  constructor() {
+    UseStatic.count += 1;
+  }
+
+  public static itStaticMethod() {
+    console.log("Run static method");
+  }
+
+  public showCount() {
+    console.log(UseStatic.count);
+  }
+}
+
+const obj1 = new UseStatic();
+const obj2 = new UseStatic();
+const obj3 = new UseStatic();
+
+obj1.showCount();
+obj2.showCount();
+obj3.showCount();
+
+UseStatic.itStaticMethod();
